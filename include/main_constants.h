cfe9d432fb4a2d7aecb8f7072ba3d984b7310087
Danil Fedotov <144121808+sh4rrkyyyy@users.noreply.github.com>
2025-02-12 01:10:49 +0300

    add main_constants.h

---
+++ include/main_constants.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+namespace main_constants{
+    const size_t arguments = 6;
+    const std::string ARCHIVED_MODE = "-c";
+    const std::string UNARCHIVED_MODE = "-u";
+    const std::string INPUT_F_MODE = "-f";
+    const std::string INPUT_FILE_MODE = "--file";
+    const std::string INPUT_O_MODE = "-o";
+    const std::string INPUT_OUTPUT_MODE = "--output";
+    const std::string OPEN_FILE_ERROR = "Can not open file";
+}
